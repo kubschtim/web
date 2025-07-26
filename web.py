c664344c20ff7e32a7434cb4052577eb374e0e5d
kubschtim <kubschtim@gmail.com>
2025-07-26 09:59:12 +0200

    learned about regex and how to scan html code for words

--- web.py
+++ web.py
@@ -1,10 +1,17 @@
+import re
 import requests
 
-url = "https://www.w3schools.com/python/demopage.htm"
-response = requests.get(url)
+url = "https://reonic.com/"
+response = requests.get(url).text
 
-print("Request-Header:")
-print(response.request.headers)
-print("-----------------------")
-print("Response-Header:")
-print(response.headers)
+#print Request and Response Header
+#print("Request-Header:")
+#print(response.request.headers)
+#print("-----------------------")
+#print("Response-Header:")
+#print(response.headers)
+
+reg = re.sub("<[^>]*>", " ", response)
+words = re.findall(r"\b\w+\b", reg)
+count = words.count("Wärmepumpen")
+print(count)

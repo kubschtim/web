e8fcab2365da07ef4143e325c953e4795415dfa9
kubschtim <kubschtim@gmail.com>
2025-07-27 12:08:02 +0200

    exploring how sub pages work

--- web.py
+++ web.py
@@ -1,17 +1,34 @@
 import re
 import requests
+import webbrowser
 
-url = "https://reonic.com/"
+url = "https://www.autarc.energy"
 response = requests.get(url).text
 
-#print Request and Response Header
+#PRINT Request and Response Header
+#-----------------------------------
 #print("Request-Header:")
 #print(response.request.headers)
 #print("-----------------------")
 #print("Response-Header:")
 #print(response.headers)
 
-reg = re.sub("<[^>]*>", " ", response)
-words = re.findall(r"\b\w+\b", reg)
-count = words.count("Wärmepumpen")
-print(count)
+#Count occurence of the word "Wärmepumpen" on a website
+#-----------------------------------
+#reg = re.sub("<[^>]*>", " ", response)
+#words = re.findall(r"\b\w+\b", reg)
+#count = words.count("Wärmepumpen")
+
+subpages = re.findall('href="([^"]+)"', response)
+
+liste = []
+
+for element in subpages:
+    if element.startswith("/") and not element.startswith("//"):
+        liste.append(element)
+
+response_subpage = requests.get(url + liste[4]).text
+
+print(response_subpage)
+
+#print(subpages)
